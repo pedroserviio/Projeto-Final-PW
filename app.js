3f3b82100f3f9c992c2979b424c1521b43425456
pedroserviio <pedroservio2010@gmail.com>
2024-06-15 12:12:57 -0300

    Adicionado cadastro de usuário ao banco

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const usuarioRoutes = require('./src/routes/usuarioRoutes');
 
 function initMiddleware() {
     app.use(express.static(path.join(__dirname , 'src', 'views')));
+    app.use(express.urlencoded({ extended: true }));
     app.use(express.json());
     app.use(session(
         {
--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -2,16 +2,19 @@ const Usuario = require('../models/Usuario');
 
 async function cadastrar_usuario(req, res) {
     try {
-        const usuario = new Usuario ({
-            nome: req.body.name,
+        console.log(req.body);
+        const usuario = new Usuario({
+            nome: req.body.nome,
             email: req.body.email,
             senha: req.body.senha
         });
 
         await usuario.save();
-        res.status(200).send({sucess : 1})
+        req.session.message = 'Cadastro realizado com sucesso! Faça login para continuar.';
+        res.redirect('/login'); // Redireciona para a página de login após o cadastro
     } catch (error) {
         console.log('Erro: ', error);
+        res.status(500).send({ error: 'Erro ao cadastrar usuário' });
     }
 }
 
